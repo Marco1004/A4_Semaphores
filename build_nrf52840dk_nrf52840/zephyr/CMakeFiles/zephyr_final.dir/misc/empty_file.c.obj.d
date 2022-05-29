b994033bf68cd280ce154a26aec5ef8573e5e068
daniel85111 <94072978+daniel85111@users.noreply.github.com>
2022-05-29 16:46:07 +0100

    Delete build_nrf52840dk_nrf52840 directory

--- build_nrf52840dk_nrf52840/zephyr/CMakeFiles/zephyr_final.dir/misc/empty_file.c.obj.d
+++
@@ -1,4 +0,0 @@
-zephyr/CMakeFiles/zephyr_final.dir/misc/empty_file.c.obj: \
- C:/Users/marco/ncs/v1.9.0/zephyr/misc/empty_file.c \
- D:/2021-2022/SETR/Pratica/A4_Semaphores/build_nrf52840dk_nrf52840/zephyr/include/generated/autoconf.h \
- C:/Users/marco/ncs/v1.9.0/zephyr/include/toolchain/zephyr_stdint.h
