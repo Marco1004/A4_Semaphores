f48c51cd07be770c7f83af33e82c0710dacba822
daniel85111 <94072978+daniel85111@users.noreply.github.com>
2022-05-30 21:04:47 +0100

    PWM aplicado

--- src/main.c
+++ src/main.c
@@ -12,8 +12,10 @@
 
 #include <zephyr.h>
 #include <device.h>
+#include <devicetree.h>
 #include <drivers/gpio.h>
 #include <drivers/adc.h>
+#include <drivers/pwm.h>
 #include <sys/printk.h>
 #include <sys/__assert.h>
 #include <string.h>
@@ -35,24 +37,12 @@
 /* Note also that the configuration of differnt channels is completely independent (gain, resolution, ref voltage, ...) */
 #define ADC_CHANNEL_INPUT NRF_SAADC_INPUT_AIN1 
 
-#define BUFFER_SIZE 1
-#define VECTOR_SIZE 10                 //------------------FILTER LOCAL VECTOR SIZER-----------------
+
 
 /* Other defines */
 #define TIMER_INTERVAL_MSEC 1000 /* Interval between ADC samples */
-
-/* ADC channel configuration */
-static const struct adc_channel_cfg my_channel_cfg = {
-	.gain = ADC_GAIN,
-	.reference = ADC_REFERENCE,
-	.acquisition_time = ADC_ACQUISITION_TIME,
-	.channel_id = ADC_CHANNEL_ID,
-	.input_positive = ADC_CHANNEL_INPUT
-};
-
-struct k_timer my_timer;
-const struct device *adc_dev = NULL;
-static uint16_t adc_sample_buffer[BUFFER_SIZE];
+#define BUFFER_SIZE 1
+#define VECTOR_SIZE 10           /* Filter Local-Vector Size */
 
 /* Size of stack area used by each thread (can be thread specific, if necessary)*/
 #define STACK_SIZE 1024
@@ -65,10 +55,17 @@ static uint16_t adc_sample_buffer[BUFFER_SIZE];
 /* Thread periodicity (in ms)*/
 #define SAMP_PERIOD_MS 1000     //---------------------SAMPLING PERIOD----------------------
 
+/* Refer to dts file */
+#define GPIO0_NID DT_NODELABEL(gpio0) 
+#define PWM0_NID DT_NODELABEL(pwm0) 
+#define BOARDLED_PIN 0xD
+
 /* Create thread stack space */
 K_THREAD_STACK_DEFINE(thread_In_stack, STACK_SIZE);
 K_THREAD_STACK_DEFINE(thread_Filter_stack, STACK_SIZE);
 K_THREAD_STACK_DEFINE(thread_Out_stack, STACK_SIZE);
+
+
   
 /* Create variables for thread data */
 struct k_thread thread_In_data;
@@ -83,6 +80,8 @@ k_tid_t thread_Out_tid;
 /* Global vars (shared memory) */
 int sm_1 = 0;
 int sm_2 = 0;
+const struct device *gpio0_dev;         /* Pointer to GPIO device structure */
+const struct device *pwm0_dev;          /* Pointer to PWM device structure */
 
 /* Semaphores for task synch */
 struct k_sem sem1;
@@ -92,6 +91,24 @@ struct k_sem sem2;
 void Input(void *argA , void *argB, void *argC);
 void Filter(void *argA , void *argB, void *argC);
 void Output(void *argA , void *argB, void *argC);
+
+/* PWM configuration */
+unsigned int pwmPeriod_us = 1000;       /* PWM priod in us */
+unsigned int dcValue = 33;              /* Duty-cycle in % */
+
+
+/* ADC channel configuration */
+static const struct adc_channel_cfg my_channel_cfg = {
+	.gain = ADC_GAIN,
+	.reference = ADC_REFERENCE,
+	.acquisition_time = ADC_ACQUISITION_TIME,
+	.channel_id = ADC_CHANNEL_ID,
+	.input_positive = ADC_CHANNEL_INPUT
+};
+
+struct k_timer my_timer;
+const struct device *adc_dev = NULL;
+static uint16_t adc_sample_buffer[BUFFER_SIZE];
 
 /* Takes one sample */
 static int adc_sample(void)
@@ -130,14 +147,32 @@ void config(void){
         printk("adc_channel_setup() failed with error code %d\n", err);
     }
     
-    /* It is recommended to calibrate the SAADC at least once before use, and whenever the ambient temperature has changed by more than 10 °C */
+    /* It is recommended to calibrate the SAADC at least once before use, and whenever the ambient temperature has changed by more than 10 ï¿½C */
     NRF_SAADC->TASKS_CALIBRATEOFFSET = 1;
-   
+    
+    /* Bind to GPIO 0 and PWM0 */
+    gpio0_dev = device_get_binding(DT_LABEL(GPIO0_NID));
+    if (gpio0_dev == NULL) {
+        printk("Error: Failed to bind to GPIO0\n\r");        
+	return;
+    }
+    else {
+        printk("Bind to GPIO0 successfull \n\r");        
+    }
+
+    pwm0_dev = device_get_binding(DT_LABEL(PWM0_NID));
+    if (pwm0_dev == NULL) {
+	printk("Error: Failed to bind to PWM0\n");
+	return;
+    }
+    else  {
+        printk("Bind to PWM0 successful\n");            
+    }
 }
 
 /* Main function */
 void main(void) {
-    
+
     config();
     
     /* Create and init semaphores */
@@ -179,8 +214,10 @@ void Input(void *argA , void *argB, void *argC)
 
     /* Thread loop */
     while(1) {
-        printk("\x1b[2J");                                    /* Clear screen */
+        
         printk("\x1b[H");   // Send cursor to home
+        printk("\x1b[2J");  /* Clear screen */
+    
         /* Do the workload */
         err=adc_sample();
         if(err) {
@@ -191,14 +228,21 @@ void Input(void *argA , void *argB, void *argC)
                 printk("adc reading out of range\n\r");
             }
             else {
-                input= (uint16_t)(1000*adc_sample_buffer[0]*((float)3/1023));
+                input = (uint16_t)(1000*adc_sample_buffer[0]*((float)3/1023));
                 /* ADC is set to use gain of 1/4 and reference VDD/4, so input range is 0...VDD (3 V), with 10 bit resolution */
                 //printk("adc reading: raw:%4u / %4u mV: \n\r",adc_sample_buffer[0],(uint16_t)(1000*adc_sample_buffer[0]*((float)3/1023)));
             }
         }
         
         sm_1= input;
+        
+        printk("PWM DC value set to %u %%\n\r",dcValue);
 
+        err = pwm_pin_set_usec(pwm0_dev, BOARDLED_PIN,
+                  pwmPeriod_us,(unsigned int)((pwmPeriod_us*dcValue)/100), PWM_POLARITY_NORMAL);
+        if (err) {
+            printk("Error %d: failed to set pulse width\n", err);
+        }
         k_sem_give(&sem1);
 
         /* Wait for next release instant */ 
@@ -269,7 +313,8 @@ void Output(void *argA , void *argB, void *argC)
     while(1) {
         k_sem_take(&sem2,  K_FOREVER);
         output= sm_2;
-        printk("Output = %d", output);
+        dcValue = (100*output)/3000;
+        printk("Output = %d\n", output);
         
   }
 }
